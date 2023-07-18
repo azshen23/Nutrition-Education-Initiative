0d52af34f5b0253c3f18b76dc02bf0da3eb27b6a
Andrew Shen <azshen23@gmail.com>
2023-07-18 08:56:36 -0400

    about page base finished

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,10 +1,80 @@
+<script>
+	import { fade } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let showPopup = false;
+
+	function togglePopup() {
+		showPopup = !showPopup;
+	}
+
+	onMount(() => {
+		// @ts-ignore
+		function handleOutsideClick(event) {
+			if (!event.target.closest('.popup-container') && !event.target.closest('.toggle-button')) {
+				showPopup = false;
+			}
+		}
+
+		// @ts-ignore
+		document.addEventListener('click', handleOutsideClick);
+
+		return () => {
+			document.removeEventListener('click', handleOutsideClick);
+		};
+	});
+</script>
+
 <div class="flex flex-col justify-between items-center pt-20 pb-10 border-t bg-white">
 	<h1 class="text-5xl font-bold pb-20">Start Your Nutrition Journey</h1>
-	<form>
-		<button class="rounded-lg border-2 border-primary pl-10 pr-10 pb-5 pt-5">
-			Enter your email to receive updates<button />
-		</button>
-	</form>
+
+	<button
+		class="rounded-lg border-2 border-primary pl-10 pr-10 pb-5 pt-5"
+		on:click|stopPropagation={togglePopup}
+	>
+		Enter your email to receive updates<button />
+	</button>
+
+	{#if showPopup}
+		<div
+			transition:fade
+			class="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center"
+		>
+			<div class="bg-white rounded pl-40 pr-40 pb-10 pt-10 popup-container relative">
+				<button
+					class="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+					on:click={togglePopup}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M3.646 3.646a.5.5 0 01.708 0L10 9.293l5.646-5.647a.5.5 0 01.708.708L10.707 10l5.647 5.646a.5.5 0 01-.708.708L10 10.707l-5.646 5.647a.5.5 0 01-.708-.708L9.293 10 3.646 4.354a.5.5 0 010-.708z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
+				<h1 class="text-2xl font-bold pb-10">Join Our Mailing List Today To Recieve Updates!</h1>
+				<input
+					type="email"
+					placeholder="Email address"
+					class="w-full p-2 pr-40 border rounded mb-4"
+				/>
+				<div class="flex row">
+					<button
+						class="pb-4 pt-4 bg-primary w-full text-white rounded hover:bg-accent"
+						on:click={togglePopup}
+					>
+						Subscribe!
+					</button>
+				</div>
+			</div>
+		</div>
+	{/if}
 </div>
 
 <style lang="postcss">
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,7 @@
 	import Header from '../../components/header.svelte';
 	import Footer from '../../components/footer.svelte';
 	import KidBanner from '../../images/about-us/parent-feeding-kid.jpg';
+	import President from '../../images/about-us/president.jpeg';
 
 	import { onMount } from 'svelte';
 	onMount(() => {
@@ -49,27 +50,95 @@
 		</div>
 		<div class="flex flex-col w-full pb-32">
 			<div class="flex flex-col w-full items-center">
-				<h1 id="mission" class="text-5xl font-bold pt-40 pb-10">Our Mission</h1>
-				<div class="flex flex-row justify-between w-full pl-12 pr-32">
-					<div class="flex flex-col">
+				<h1 id="mission" class="text-5xl font-bold pt-10 pb-20">Our Mission</h1>
+				<div class="flex flex-row w-full pl-32 pr-32 gap-8">
+					<div class="flex-1 flex-col rounded-2xl bg-secondary pb-20 pt-10 pl-5 pr-5">
 						<h1 class="text-3xl text-center pb-8">Empowering</h1>
 						<p class="text-center">
 							We believe in empowering students to make informed choices about their nutrition and
 							overall health.
 						</p>
 					</div>
-					<div class="flex flex-col">
+					<div class="flex-1 flex-col rounded-2xl bg-secondary pb-20 pt-10 pl-5 pr-5">
 						<h1 class="text-3xl text-center pb-8">Accessible</h1>
 						<p class="text-center">
 							N.E.I. provides accessible education to rural high school students, ensuring that
 							everyone has the opportunity to learn about nutrition.
 						</p>
 					</div>
-					<div class="flex flex-col">
+					<div class="flex-1 flex-col rounded-2xl bg-secondary pb-20 pt-10 pl-5 pr-5">
 						<h1 class="text-3xl text-center pb-8">Healthy Habits</h1>
 						<p class="text-center">
 							We promote healthy habits and encourage students to prioritize their well-being.
 						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="flex flex-row w-full pl-48 pr-48 pt-16 pb-32">
+			<div class="flex flex-col w-full items-center">
+				<h1 class="text-5xl font-bold pb-16">Why are we here?</h1>
+				<p class="text-lg text-center pb-20">
+					The NEI was initiated to address the rising obesity rates, and related obesity sequelae
+					such as stroke, heart disease, diabetes, and cancer. Obesity is a multifactorial disorder
+					and requires a multidisciplinary approach in combating its epidemic. To date, the NEI has
+					supplied over 20 hours of free nutrition education to the Mount Pleasant and Beal City
+					Public Schools and plans to expand to neighboring rural high schools. The NEI currently
+					has an IRB approved study that will examine if high school student behaviors will change
+					after the intervention. The NEI has three medical school faculty advisors who have
+					provided oversight over current research: Dr. Ariel Cascio, Dr. Jesse Bakke, and Dr.
+					Samantha Hahn. Nathan Shen, a current M3 medical student, has currently led the NEI with
+					support of fellow medical students.
+				</p>
+			</div>
+		</div>
+		<div class="flex flex-row w-full pb-32">
+			<div class="flex flex-col w-full items-center pl-20 pr-20">
+				<h1 class="text-5xl font-bold pb-16">Meet our team!</h1>
+				<div class="flex flex-row w-full items-center text-center">
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="President" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">President</h1>
+					</div>
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="Vice President" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">Vice President</h1>
+					</div>
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="Secretary" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">Secretary</h1>
+					</div>
+				</div>
+				<div class="flex flex-row w-full items-center text-center">
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="Treasurer" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">Treasurer</h1>
+					</div>
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="Other Person 1" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">Other Person 1</h1>
+					</div>
+					<div class="flex-1 flex-col">
+						<div class="flex items-center justify-center pb-10">
+							<img src={President} alt="Other Person 2" class="w-80 h-80 rounded-3xl" />
+						</div>
+
+						<h1 class="text-3xl text-center pb-8">Other Person 2</h1>
 					</div>
 				</div>
 			</div>
