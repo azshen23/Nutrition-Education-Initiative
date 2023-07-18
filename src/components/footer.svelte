7adf41416542d9d8070bf19544f12c3b15273a0b
Andrew Shen <azshen23@gmail.com>
2023-07-17 21:51:30 -0400

    Update footer.svelte

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,9 @@
 <div class="flex flex-col justify-between items-center pt-10 pb-10 border-t bg-white">
-	<h1 class="text-xl font-bold">Start Your Nutrition Journey</h1>
+	<h1 class="text-5xl font-bold pb-20">Start Your Nutrition Journey</h1>
 	<form>
-		<input type="text" class="rounded-lg border border-black" />
+		<button class="rounded-lg border-2 border-primary pl-10 pr-10 pb-5 pt-5">
+			Enter your email to receive updates<button />
+		</button>
 	</form>
 </div>
 
