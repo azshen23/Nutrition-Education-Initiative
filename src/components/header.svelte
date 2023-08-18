3e7a211c4f63921729ef32f874e9ebf33922e272
Andrew Shen <azshen23@gmail.com>
2023-08-18 00:33:32 -0400

    Update header.svelte

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -48,7 +48,7 @@
 		</div>
 	</div>
 	<div
-		class="hidden sticky top-0 sm:flex flex-row justify-between items-center pl-20 pr-20 pt-5 pb-5 bg-white border-b"
+		class="hidden sm:flex flex-row justify-between items-center pl-20 pr-20 pt-5 pb-5 bg-white border-b"
 	>
 		<a href="/">
 			<img src={logoNoBg} alt="NEI logo" width="150" height="150" />
